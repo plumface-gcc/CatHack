0a48e478f387901866f60aa7375939dadf110450
thatr <thatr@DESKTOP-CH6T0C6>
2021-02-09 09:20:44 -0500

    saving and committing because im a bitch

--- CatHack/CatHack/CatHack/CatHackOrbWalk.cs
+++ CatHack/CatHack/CatHack/CatHackOrbWalk.cs
@@ -8,21 +8,9 @@ namespace CatHack
 {
     class CatHackOrbWalk
     {
-        private static short keyState;
-        private static int userPing;
         private static int LastAATick;
         private static int LastAATick2;
-        private static float userPingFloat;
         private static int LastMoveCommandT;
-        private static bool IsOrbAttackable = true;
-
-        private static float attackSpeed;
-        private static float tAttackCooldown;
-        private static float tAttackWindup;
-
-        private static float WindupPercent;
-        private static float bWindupTime;
-        private static float cAttackTime;
 
         private static Point LastMovePoint;
 
@@ -32,42 +20,9 @@ namespace CatHack
         /// <summary>
         /// Handles calculating the current champion's attack windup
         /// </summary>
-        /// <returns>tAttackWindupFinal</returns>
+        /// <returns>Attack Windup</returns>
+      
         public static int getAttackWindup()
-        {
-            CatHackMain cathack = new CatHackMain();
-
-            attackSpeed = LocalPlayer.GetAttackSpeed();
-
-            userPing = (int)userPingFloat;
-            tAttackCooldown = (1 / attackSpeed) * 1000;
-
-            try
-            {
-                int tAttackCooldownFinal = Convert.ToInt32(tAttackCooldown);
-            }
-            catch (OverflowException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
-            float champWindupPercent = cathack.getWindupPercent();
-            float champBaseWindupTime = cathack.getBaseWindupTime();
-            float champWindupModifier = cathack.getWindupModifier();
-
-            cAttackTime = 1 / attackSpeed;
-            WindupPercent = (champWindupPercent) / 100;
-            bWindupTime = (1 / champBaseWindupTime) * WindupPercent;
-            tAttackWindup = bWindupTime + ((cAttackTime * WindupPercent) - bWindupTime) * champWindupModifier;
-
-            tAttackWindup *= 1000;
-
-            int tAttackWindupFinal = Convert.ToInt32(tAttackWindup);
-
-            return tAttackWindupFinal;
-        }
-
-        public static int getAttackWindupTest()
         {
             CatHackMain cathack = new CatHackMain();
             float champWindupPercent = cathack.getWindupPercent();
@@ -90,7 +45,7 @@ namespace CatHack
 
         public static bool CanMove()
         {
-            return LastMoveCommandT < Environment.TickCount;
+            return LastMoveCommandT <= Environment.TickCount;
         }
 
         /// <summary>
@@ -109,20 +64,16 @@ namespace CatHack
                 if (CanAttack() && enemyPos != Point.Empty)
                 { 
                     LastMovePoint = Cursor.Position; // save original position of cursor before attacking an enemy
-
                     KeyMouseHandler.IssueOrder(OrderEnum.AttackUnit, enemyPos); // attack le enemy
-                    KeyMouseHandler.IssueOrder(OrderEnum.AttackUnit, enemyPos);
 
-                    LastAATick2 = (int)((ApiHandler.GetGameTime() * 1000) + getAttackWindupTest() + SaveUserPing.getUserPing());
-
+                    LastAATick2 = (int)((ApiHandler.GetGameTime() * 1000) + getAttackWindup() + SaveUserPing.getUserPing());
                     LastAATick = Environment.TickCount;              
-                    LastMoveCommandT = Environment.TickCount + getAttackWindupTest();
+                    LastMoveCommandT = Environment.TickCount + getAttackWindup();
                 }
                 if (CanMove() && enemyPos != Point.Empty)
                 {
                     KeyMouseHandler.IssueOrder(OrderEnum.MoveTo, LastMovePoint);
                     LastMovePoint = Cursor.Position;
-
                     LastMoveCommandT = Environment.TickCount + rnd.Next(50, 80);
                 }
                 else
--- CatHack/CatHack/CatHack/userPingStuff/SaveUserPing.cs
+++ CatHack/CatHack/CatHack/userPingStuff/SaveUserPing.cs
@@ -21,41 +21,41 @@ namespace CatHack
 
         public SaveUserPing(Int32 x, Int32 y, Int32 w, Int32 h, Size s)
         {
-            InitializeComponent();
+                InitializeComponent();
 
-            while (loop)
-            {
-                System.Threading.Thread.Sleep(700);
-
-                Rectangle rectRecurse = new Rectangle(x, y, w, h);
-                Bitmap bmpRecurse = new Bitmap(rectRecurse.Width, rectRecurse.Height, PixelFormat.Format32bppArgb);
-                Graphics newGraphic = Graphics.FromImage(bmpRecurse);
-                newGraphic.CopyFromScreen(rectRecurse.Left, rectRecurse.Top, 0, 0, s, CopyPixelOperation.SourceCopy);
-                bmpRecurse.Save(@"C:\Users\" + userName + @"\Documents\recurseImg2.bmp", System.Drawing.Imaging.ImageFormat.Jpeg);
-                imageCapture.Image = bmpRecurse;
-
-                var ocr = new TesseractEngine(@"C:\Users\" + userName + @"\Documents\tessdata", "eng", EngineMode.LstmOnly);
-                ocr.SetVariable("tessedit_char_whitelist", "0123456789");
-
-                var img = Pix.LoadFromFile(@"C:\Users\" + userName + @"\Documents\recurseImg2.bmp");
-                var res = ocr.Process(img, PageSegMode.SingleBlock);
-
-                var final = res.GetText();
-                ocr.Dispose();
-                bmpRecurse.Dispose();
-
-                if (rgx.IsMatch(final))
+                while (loop)
                 {
-                    try
+                    System.Threading.Thread.Sleep(1000);
+
+                    Rectangle rectRecurse = new Rectangle(x, y, w, h);
+                    Bitmap bmpRecurse = new Bitmap(rectRecurse.Width, rectRecurse.Height, PixelFormat.Format32bppArgb);
+                    Graphics newGraphic = Graphics.FromImage(bmpRecurse);
+                    newGraphic.CopyFromScreen(rectRecurse.Left, rectRecurse.Top, 0, 0, s, CopyPixelOperation.SourceCopy);
+                    bmpRecurse.Save(@"C:\Users\" + userName + @"\Documents\recurseImg2.bmp", System.Drawing.Imaging.ImageFormat.Jpeg);
+                    imageCapture.Image = bmpRecurse;
+
+                    var ocr = new TesseractEngine(@"C:\Users\" + userName + @"\Documents\tessdata", "eng", EngineMode.LstmOnly);
+                    ocr.SetVariable("tessedit_char_whitelist", "0123456789");
+
+                    var img = Pix.LoadFromFile(@"C:\Users\" + userName + @"\Documents\recurseImg2.bmp");
+                    var res = ocr.Process(img, PageSegMode.SingleBlock);
+
+                    var final = res.GetText();
+                    ocr.Dispose();
+                    bmpRecurse.Dispose();
+
+                    if (rgx.IsMatch(final))
                     {
-                        userPing = float.Parse(final);
+                        try
+                        {
+                            userPing = float.Parse(final);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
                     }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.Message);
-                    }
-                }
-            }
+                }       
         }
 
         public static float getUserPing()
